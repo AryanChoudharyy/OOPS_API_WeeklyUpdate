1d17852011fa82f35c1e32f7c25a039c29aa28bb
AryanChoudharyy <140917288+AryanChoudharyy@users.noreply.github.com>
2024-04-08 23:51:12 +0530

    first commit

---
+++ ProductService-main/src/main/java/com/sst/productservice/services/FakeStoreProductService.java
@@ -0,0 +1,59 @@
+package com.sst.productservice.services;
+
+import com.sst.productservice.dtos.FakeStoreProductDto;
+import com.sst.productservice.exceptions.ProductNotFoundException;
+import com.sst.productservice.models.Category;
+import com.sst.productservice.models.Product;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FakeStoreProductService implements ProductService{
+    public Product getProductById(Long id) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        FakeStoreProductDto fakeStoreProductDto = restTemplate.getForObject(
+                "https://fakestoreapi.com/products/" + id,
+                FakeStoreProductDto.class
+        );
+
+        if (fakeStoreProductDto == null)
+            throw new ProductNotFoundException(id, "Please provide a valid id");
+
+        return convertFakeStoreProductDtoToProduct(fakeStoreProductDto);
+    }
+
+    public List<Product> getAllProducts() {
+        RestTemplate restTemplate = new RestTemplate();
+        List<Product> allProducts = new ArrayList<>();
+
+        FakeStoreProductDto[] fakeStoreProductDtos = restTemplate.getForObject(
+                "https://fakestoreapi.com/products",
+                FakeStoreProductDto[].class
+        );
+
+        for (FakeStoreProductDto fakeStoreProductDto : fakeStoreProductDtos) {
+            allProducts.add(convertFakeStoreProductDtoToProduct(fakeStoreProductDto));
+        }
+
+        return allProducts;
+    }
+
+    public Product convertFakeStoreProductDtoToProduct(FakeStoreProductDto fakeStoreProductDto) {
+        Product product = new Product();
+
+        product.setId(fakeStoreProductDto.getId());
+        product.setTitle(fakeStoreProductDto.getTitle());
+        product.setDescription(fakeStoreProductDto.getDescription());
+        product.setPrice(fakeStoreProductDto.getPrice());
+        Category category = new Category();
+        category.setDescription(fakeStoreProductDto.getCategory());
+        product.setCategory(category);
+        product.setImage(fakeStoreProductDto.getImage());
+
+        return product;
+    }
+}
